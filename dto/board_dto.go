bca4a587cd654469c96130ffb9e6f7cb10a9b0c2
fum-is-chum <alvinardiansyah2002@gmail.com>
2023-10-22 22:11:27 +0700

    feat: re-add Owner, move BoardMemberResponse to separate dto file

--- dto/board_dto.go
+++ dto/board_dto.go
@@ -1,7 +1,5 @@
 package dto
 
-import "time"
-
 type BoardRequest struct {
 	Name string `json:"name,omitempty" validate:"required"`
 	Desc string `json:"desc,omitempty" validate:"required"`
@@ -11,14 +9,7 @@ type BoardResponse struct {
 	ID      uint                   `json:"id"`
 	Name    string                 `json:"name"`
 	Desc    string                 `json:"desc"`
-	Members []*MemberResponse      `json:"members"`
+	Owner   *BoardMemberResponse   `json:"owner"`
+	Members []*BoardMemberResponse `json:"members"`
 	Columns []*BoardColumnResponse `json:"columns"`
-}
-
-type MemberResponse struct {
-	ID        uint      `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
 }
--- model/user_model.go
+++ model/user_model.go
@@ -7,6 +7,6 @@ type User struct {
 	Name     string   `json:"name,omitempty"`
 	Email    string   `json:"email,omitempty" gorm:"unique"`
 	Password string   `json:"password,omitempty"`
-	Boards   []Board  `json:"boards" gorm:"foreignkey:OwnerID;constraint:OnDelete:CASCADE;"`
+	Boards   []*Board `json:"boards" gorm:"foreignkey:OwnerID;constraint:OnDelete:CASCADE;"`
 	MemberOf []*Board `json:"member_of" gorm:"many2many:board_members;"`
 }
